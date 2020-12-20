d3b069665a6bbf7f9f3a6a4392f11a5a006d1566
Xavier <52459675+XavierTheCreator@users.noreply.github.com>
2020-12-20 17:25:30 -0500

    Add files via upload

---
+++ CPUINFO.py
@@ -0,0 +1,18 @@
+import psutil 
+
+#Checks for number of cores 
+
+print('Physical Cores: ',psutil.cpu_count(logical=False))
+print('Total Cores: ', psutil.cpu_count(logical=True))
+
+
+#CPU Frequecies
+cpufreq = psutil.cpu_freq()
+print(f'Max Freq: {cpufreq.max:.2f}Mhz ')
+print(f'Current Freq: {cpufreq.current:.2f}Mhz ')
+
+#Usage
+print('CPU Usage Per Core: ')
+for i, percentage in enumerate(psutil.cpu_percent(percpu=True,interval = 1)):
+    print(f"Core{i}: {percentage}%")
+print(f'Total CPU Usage: {psutil.cpu_percent()}%')
